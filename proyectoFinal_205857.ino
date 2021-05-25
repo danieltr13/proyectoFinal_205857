b101f081bb6ba296027127c3a4f87e01fd720f1a
danieltr13 <daniel.trinidad205857@potros.itson.edu.mx>
2021-05-25 02:02:03 -0700

    arduino ready

--- proyectoFinal_205857.ino
+++ proyectoFinal_205857.ino
@@ -28,7 +28,9 @@ int estado;
 //pausas
 const int PAUSA = 500;
 const int DURACION = 3000;
-int lapsoAnterior = 0;
+const long PERIODO = 3000;
+
+unsigned long lapsoAnterior = 0;
 unsigned long lapsoActual = 0;
 
 //comunicación
@@ -73,8 +75,17 @@ int anguloVentanaAutomatica = 0;
 
 //Intencidad led
 const int IBAJA = 1, IMEDIO = 2, IALTO = 3;
-int intencidad;
-bool estadoIntencidad=true;
+int intencidad = IBAJA;
+int intencidadAnterior = IBAJA;
+
+bool estadoIntencidad = true;
+
+const int LED_ON = 1, LED_OFF = 2, LED_AUTO = 3;
+int estadoLED = LED_AUTO;
+bool aEstadoLed = false;
+bool aEstadoLedAnterior = false;
+
+bool estadoRGB_actual = false;
 
 void setup() {
   Serial.begin(BAUD_RATE);
@@ -94,6 +105,7 @@ void setup() {
   debouncer.attach(PIN_BOTON_IZQUIERDA);
   debouncerDer.attach(PIN_BOTON_DERECHA);
   debouncerButton.attach(PIN_BOTON);
+
   // Establece el intervalo (en ms) de espera para eliminar el
   // ruido
   debouncer.interval(25);
@@ -107,10 +119,12 @@ void setup() {
 
 void loop() {
   char comando[10];
+  //Se actualiza el estado de los debouncer
   debouncer.update();
   debouncerDer.update();
   debouncerButton.update();
 
+  //se verifica si es que fueron presionados
   if (debouncer.fell()) {
     Serial.println("izquierda");
     ventanaAutomatica = false;
@@ -125,105 +139,189 @@ void loop() {
     cambiarEstadoRGB();
   }
 
+  //Si el estado la luz esta en automatico entonces se evaluará si existe la presencia de una persona
   if (estado_luz == LAUTO) {
     int valorPir = digitalRead(PIN_PIR);
     // Si se detecta movimiento
     if (valorPir == HIGH) {
       // se enciende el LED
       estadoLEDS();
-      // Si no habia movimiento anteriormente
       if (estadoAntPir == LOW) {
-        Serial.println("Se detecto movimiento");
-        // Hay movimiento
+        Serial.println("Movimiento");
+        estadoRGB_actual = true;
         estadoAntPir = HIGH;
       }
     }
     // Si no se detecto movimiento
     else {
       // Apaga el LED
+      estadoRGB_actual = false;
       RGB_color(0, 0, 0);
       // Si habia movimiento anteriormente
       if (estadoAntPir == HIGH) {
-        Serial.println("No se detecto movimiento");
-        // No hay movimiento
         estadoAntPir = LOW;
       }
     }
   }
+  //la intencidad se puede controlar de manera automatica o establecer de manera manual
+  // Si se encuentra automatica entonces revisa el nivel de luz
   if (estadoIntencidad) {
     int nivelLuzRGB = analogRead(PIN_FOTO_RGB);
     float valorF = nivelLuzRGB * (5.0 / 1023.0);
-    Serial.print("FRGB-");
-    Serial.println(valorF);
     cambiarIntencidad(valorF);
   }
+
+  //Se toma el valor de la intencidad
   int nivelLuz = analogRead(PIN_SENSOR);
   float valor = nivelLuz * (5.0 / 1023.0);
-  Serial.println(valor);
-  if (valor < .90) {
-    digitalWrite(PIN_LED_LUZ, HIGH);
+  if (valor < .85) {
+    aEstadoLed = true;
     anguloVentanaAutomatica = 180;
   } else {
-    digitalWrite(PIN_LED_LUZ, LOW);
+    aEstadoLed = false;
     anguloVentanaAutomatica = 45;
+  }
+
+  if (aEstadoLed != aEstadoLedAnterior) {
+    aEstadoLedAnterior = aEstadoLed;
+    cambiarEstadoLed();
+    Serial.println(valor);
+  } else {
+    long lapsoActual = millis();
+    if (lapsoActual - lapsoAnterior >= PERIODO) {
+      Serial.println(valor);
+      lapsoAnterior = lapsoActual;
+    }
   }
   if (ventanaAutomatica) {
     servo.write(anguloVentanaAutomatica);
   }
+
   if (Serial.available() > 0) {
     int n = Serial.readBytesUntil('\n', comando, 9);
     comando[n] = '\0';
-    if (!strcmp(comando, "LON")) {
-      estado_luz = LON;
-      estadoLEDS();
-    }
-    else if (!strcmp(comando, "LOFF")) {
-      estado_luz = LOFF;
-      RGB_color(0, 0, 0);
-    }
-    else if (!strcmp(comando, "LAUTO")) {
-      estado_luz = LAUTO;
-    }
-    else if (!strcmp(comando, "IBAJA")) {
-      intencidad = IBAJA;
-      estadoIntencidad=false;
-    }
-    else if (!strcmp(comando, "IMEDIO")) {
-      intencidad = IMEDIO;
-      estadoIntencidad=false;
-    }
-    else if (!strcmp(comando, "IALTO")) {
-      intencidad = IALTO;
-      estadoIntencidad=false;
-    }
-    else if (!strcmp(comando, "IAUTO")) {
-      estadoIntencidad=true;
-    }
+    manejarRecibido(comando);
   }
 }
 
-void cambiarIntencidad(float valorF){
-  if(valorF < .85){
-    intencidad = IALTO;
-  }else if(valorF > .80 && valorF<3){
-    intencidad = IMEDIO;
-  }
-  else if(valorF > 3){
-    intencidad = IBAJA;
+void cambiarEstadoLed() {
+  if (aEstadoLed) {
+    digitalWrite(PIN_LED_LUZ, HIGH);
+    Serial.println("LED_ON");
+  } else {
+    digitalWrite(PIN_LED_LUZ, LOW);
+    Serial.println("LED_OFF");
   }
 }
 
-void cambiarEstadoRGB() {
-  if (estado_luz == LOFF) {
+void manejarRecibido(char comando[]) {
+  if (!strcmp(comando, "LON")) {
     estado_luz = LON;
     estadoLEDS();
   }
-  else if (estado_luz == LON) {
+  else if (!strcmp(comando, "LOFF")) {
+    estado_luz = LOFF;
+    RGB_color(0, 0, 0);
+  }
+  else if (!strcmp(comando, "LAUTO")) {
     estado_luz = LAUTO;
   }
-  else if(estado_luz == LAUTO){
+  else if (!strcmp(comando, "IBAJA")) {
+    intencidad = IBAJA;
+    estadoIntencidad = false;
+  }
+  else if (!strcmp(comando, "IMEDIO")) {
+    intencidad = IMEDIO;
+    estadoIntencidad = false;
+  }
+  else if (!strcmp(comando, "IALTO")) {
+    intencidad = IALTO;
+    estadoIntencidad = false;
+  }
+  else if (!strcmp(comando, "IAUTO")) {
+    estadoIntencidad = true;
+  }
+  else if (!strcmp(comando, "LED_ON")) {
+    estadoLED = LED_ON;
+    digitalWrite(PIN_LED_LUZ, HIGH);
+    Serial.println("LED_ON");
+  }
+  else if (!strcmp(comando, "LED_OFF")) {
+    estadoLED = LED_OFF;
+    digitalWrite(PIN_LED_LUZ, LOW );
+    Serial.println("LED_OFF");
+  }
+  else if (!strcmp(comando, "LED_AUTO")) {
+    estadoLED = LED_ON;
+  }
+  else if (!strcmp(comando, "45")) {
+    ventanaAutomatica = false;
+    servo.write(45);
+    Serial.println("45");
+  }
+  else if (!strcmp(comando, "90")) {
+    ventanaAutomatica = false;
+    servo.write(90);
+    Serial.println("90");
+  }
+  else if (!strcmp(comando, "135")) {
+    ventanaAutomatica = false;
+    servo.write(135);
+    Serial.println("135");
+  }
+  else if (!strcmp(comando, "180")) {
+    ventanaAutomatica = false;
+    servo.write(180);
+    Serial.println("180");
+  }
+  else if (!strcmp(comando, "VAUTO")) {
+    ventanaAutomatica = true;
+    servo.write(45);
+  }
+}
+
+void cambiarIntencidad(float valorF) {
+  if (valorF < .85) {
+    intencidad = IALTO;
+  } else if (valorF > .80 && valorF < 3) {
+    intencidad = IMEDIO;
+  }
+  else if (valorF > 3) {
+    intencidad = IBAJA;
+  }
+  if (intencidad != intencidadAnterior) {
+    intencidadAnterior = intencidad;
+    enviarEstadoIntencidad();
+  }
+  if (estadoRGB_actual) {
+    estadoLEDS();
+  }
+}
+
+void enviarEstadoIntencidad() {
+  if (intencidad == IALTO) {
+    Serial.println("IALTO");
+  } else if (intencidad == IMEDIO) {
+    Serial.println("IMEDIO");
+  } else if (intencidad == IBAJA) {
+    Serial.println("IBAJA");
+  }
+}
+
+
+void cambiarEstadoRGB() {
+  if (estado_luz == LAUTO) {
+    estado_luz = LON;
+    estadoLEDS();
+    estadoRGB_actual = true;
+  }
+  else if (estado_luz == LON) {
     estado_luz = LOFF;
-    RGB_color(0,0,0);
+    RGB_color(0, 0, 0);
+    estadoRGB_actual = false;
+  }
+  else if (estado_luz == LOFF) {
+    estado_luz = LAUTO;
   }
 }
 
@@ -239,7 +337,7 @@ void estadoLEDS() {
 }
 
 void botonIzquierda() {
-  if (angulo > 0) {
+  if (angulo > 45) {
     angulo -= 45;
     servo.write(angulo);
   }
@@ -249,7 +347,7 @@ void botonDerecha() {
   if (angulo < 180) {
     angulo += 45;
   } else {
-    angulo = 0;
+    angulo = 45;
   }
   servo.write(angulo);
 }
